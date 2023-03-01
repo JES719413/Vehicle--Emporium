98289493b384adcac480d4ba97df863dea24f333
Jacob Erven-Smalley <je719413@southeast.edu>
2023-03-01 14:06:47 -0600

    Add project files.

---
+++ Vehicle--Emporium/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Vehicle__Emporium.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
