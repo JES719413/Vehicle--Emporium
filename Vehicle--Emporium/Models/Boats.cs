b683750332eb554688d5dfb18a99eb647345a685
Jacob Erven-Smalley <je719413@southeast.edu>
2023-03-01 14:13:02 -0600

    Added boat models

---
+++ Vehicle--Emporium/Models/Boats.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vehicle__Emporium.Models
+{
+    public class Boats
+    {
+        public int BoatId { get; set; }
+
+        [Required(ErrorMessage = "Please enter the type of Boat you have.")]
+        [Column(TypeName = "varchar(75)")]
+        public string BoatType { get; set; }
+
+        [Required(ErrorMessage = "Please enter the registration number.")]
+        [Column(TypeName = "int")]
+        public int Registration { get; set; }
+
+        [Required(ErrorMessage = "Please enter your boat's length.")]
+        [Column(TypeName = "int")]
+        public int BoatLength { get; set; }
+
+        [Required(ErrorMessage = "Please enter your boat's manufacturer.")]
+        [Column(TypeName = "varchar(75)")]
+        public string Manufacturer { get; set; }
+    }
+}
---
+++ Vehicle--Emporium/Models/RowBoat.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Vehicle__Emporium.Models
+{
+    public class RowBoat : Boats
+    {
+        [Required(ErrorMessage = "Please enter the number of rowers.")]
+        [Column(TypeName = "int")]
+        public int NumberOfRowers { get; set; }
+
+        [Required(ErrorMessage = "Please indicate weather you have a motor or not.")]
+        [Column(TypeName = "varchar(10)")]
+        public string Motor { get; set; }
+        /*Not sure on this a bit could be used here as 1 could be true 0 would be false something we can think about */
+    }
+}
