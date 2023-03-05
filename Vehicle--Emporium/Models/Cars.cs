7772bcc22fab0e136e27ebdf7bac3855f9aef8fe
Evan Ballagh <eb654594@southeast.edu>
2023-03-05 17:12:25 -0600

    added cars and motorcycles model

---
+++ Vehicle--Emporium/Models/Cars.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vehicle__Emporium.Models
+{
+    public class Cars 
+    {
+        public int carsId { get; set; }
+
+        public int vehicleID { get; set; }
+        [Required(ErrorMessage = "Please enter the car type.")]
+        [Column(TypeName = "varchar(75)")]
+        public string carType { get; set; }
+        [Required(ErrorMessage = "Please enter the fuel capcity.")]
+        [Column(TypeName = "int")]
+        public int fuelCapcity { get; set; }
+        
+
+    }
+}
---
+++ Vehicle--Emporium/Models/Motorcycles.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vehicle__Emporium.Models
+{
+    public class Motorcycles
+    {
+        public int bikeID { get; set; }
+        public int vehicleID { get; set; }
+        [Required(ErrorMessage = "Please enter the type of bike.")]
+        [Column(TypeName = "int")]
+        public string bikeType { get; set;}
+        [Required(ErrorMessage = "Please enter the type of engine.")]
+        [Column(TypeName = "varchar(75)")]
+        public string bikeEngineType { get; set; }
+        [Required(ErrorMessage = "Please enter the bike's ride height.")]
+        [Column(TypeName = "varchar(75)")]
+        public int rideHeight { get; set; }
+        [Required(ErrorMessage = "Please enter the chain type.")]
+        [Column(TypeName = "varchar(75)")]
+        public string chainType { get; set; }
+        [Required(ErrorMessage = "Please enter the chain length.")]
+        [Column(TypeName ="int")]
+        public int chainLength { get; set; }
+        [Required(ErrorMessage = "Please enter if the bike has a sidecar.")]
+        [Column(TypeName = "bool")]
+        public bool sideCar { get; set; }
+
+    }
+}
