b325938cac15eaa1d0822177ab54ef0212be383d
kg412051 <kristopherglanz@gmail.com>
2023-03-06 13:57:40 -0600

    Add BoatEngine Remove Boat Classes

--- Vehicle--Emporium/Models/Engine.cs
+++ Vehicle--Emporium/Models/Engine.cs
@@ -26,21 +26,5 @@ namespace Vehicle__Emporium.Models
         [Column(TypeName = "varchar(75)")]
         public int engineType { get; set; }
 
-        [Required(ErrorMessage = "Please enter the number of hours on the engine.")]
-        [Column(TypeName = "int")]
-        public int engineHour { get; set; }
-
-        [Required(ErrorMessage = "Please enter the drive type of the engine.")]
-        [Column(TypeName = "varchar(75)")]
-        public int engineDriveType { get; set; }
-
-        [Required(ErrorMessage = "Please enter the propeller type.")]
-        [Column(TypeName = "varchar(75)")]
-        public int propellerType { get; set; }
-
-        [Required(ErrorMessage = "Please enter the propeller material.")]
-        [Column(TypeName = "varchar(75)")]
-        public int propellerMaterial { get; set; }
-
     }
 }
