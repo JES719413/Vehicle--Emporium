0b45bd927087d30287e2b51dad45a0cb159c053f
Jacob Erven-Smalley <je719413@southeast.edu>
2023-03-22 13:04:47 -0500

    added view models for vehicle types

---
+++ Vehicle--Emporium/ViewModels/CarsViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Vehicle__Emporium.Models;
+namespace Vehicle__Emporium.ViewModels
+{
+    public class CarsViewModel
+    {
+        public Cars cars { get; set; }
+        public IFormFile? Photo { get; set; }
+    }
+}
---
+++ Vehicle--Emporium/ViewModels/TravelTrailerViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Vehicle__Emporium.Models;
+namespace Vehicle__Emporium.ViewModels
+{
+    public class TravelTrailerViewModel
+    {
+        public TravelTrailer travelTrailer { get; set; }
+        public IFormFile? Photo { get; set; }
+    }
+}
