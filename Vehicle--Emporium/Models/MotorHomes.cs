b0fcb3b2ef6cecce5bfcee6c4561f883d912c786
Jacob Erven-Smalley <je719413@southeast.edu>
2023-04-19 13:17:03 -0500

    added display names to all models

--- Vehicle--Emporium/Models/MotorHomes.cs
+++ Vehicle--Emporium/Models/MotorHomes.cs
@@ -7,18 +7,23 @@ namespace Vehicle__Emporium.Models
     public class MotorHomes : Vehicles
 
     {
+        [Display(Name = "Slide Outs: ")]
         [Required(ErrorMessage = "Please select amount of slide outs.")]
         [Column(TypeName = "int")]
         public int slideOuts { get; set; }
+        [Display(Name = "Sleeps: ")]
         [Required(ErrorMessage = "Please enter amount of people it can sleep.")]
         [Column(TypeName = "int")]
         public int sleeps { get; set; }
+        [Display(Name = "Fuel Type: ")]
         [Required(ErrorMessage = "Please select the type of fuel.")]
         [Column(TypeName = "varchar(75)")]
         public string fuelType { get; set; }
+        [Display(Name = "RV Class: ")]
         [Required(ErrorMessage = "Please select the RV class.")]
         [Column(TypeName = "varchar(75)")]
         public string rvClass { get; set; }
+        [Display(Name = "Length: ")]
         [Required(ErrorMessage = "Please enter the length of the motor home")]
         [Column(TypeName = "decimal(18,2)")]
         public decimal length { get; set; }
